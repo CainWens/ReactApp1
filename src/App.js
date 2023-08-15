3aadad76e4464517243c0d85571917f99d9c6027
CainWens <makss314@gmail.com>
2023-08-15 13:16:57 +0700

    HomeWork 1

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+
+  const userName = 'admin'
+
+  return (
+    <div className="App-header">
+      <div className="App">
+          <Myheader name={userName} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+const Myheader = ({ name }) => {
+
+  return (
+    <div>
+      <h1 className="op3">Haslo, {name}</h1>
+    </div>
+  )
+}
